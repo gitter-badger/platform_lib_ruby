0b5bb926ea9cd72bd3ebe269aeccb0690866a6d6
Michael Burns <michael.burns@shawmedia.ca>
2014-04-28 14:08:49 -0400

    Adding new class for task data service

--- lib/platform_lib.rb
+++ lib/platform_lib.rb
@@ -1,6 +1,7 @@
 require "platform_lib/version"
 require "platform_lib/data_service"
 require "platform_lib/media_service"
+require "platform_lib/task_service"
 
 # scripts
 require "scripts/sync_guid_with_id"
